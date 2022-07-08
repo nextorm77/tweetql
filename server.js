769aca5b7e3adeee720305c72ff368a451a4b4aa
nextomr77 <sonsw77@gmail.com>
2022-07-09 01:15:26 +0900

    test 4.8 Type Resolvers

--- server.js
+++ server.js
@@ -11,13 +11,26 @@ let tweets = [
   },
 ];
 
+let users = [
+  {
+    id: "1",
+    firstName: "snoo",
+    lastName: "son",
+  },
+  {
+    id: "2",
+    firstName: "Nancy",
+    lastName: "son",
+  },
+];
+
 // 사용자가 뭔가를 request하게 하려면 type Query안에 있어야 함
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
     firstName: String!
-    lastName: String
+    lastName: String!
+    fullName: String!
   }
   type Tweet {
     id: ID!
@@ -25,6 +38,7 @@ const typeDefs = gql`
     author: User
   }
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
   }
@@ -48,6 +62,10 @@ const resolvers = {
       // tweets.push(); // Query와 Mutation 영역은 개념적으로 나뉜 것 뿐. 수정 코드가 있어도 오류X
       return tweets.find((tweet) => tweet.id === id);
     },
+    allUsers() {
+      console.log("allUsers called");
+      return users;
+    },
   },
   Mutation: {
     postTweet(_, { text, userId }) {
@@ -63,6 +81,29 @@ const resolvers = {
       if (!tweet) return false;
       tweets = tweets.filter((tweet) => tweet.id !== id);
       return true;
+    },
+  },
+  /*
+  User: {
+    // 해당 DB 필드가 null이 아닌 값이 있어도
+    // 리턴 값으로 대체
+    firstName() {
+      console.log("firstName called");
+      return "DB value intercepted";
+      //return null; // Non Nullable 필드이므로 에러 발생
+    },
+    // 해당 DB 필드가 null이어서 관련 에러 발생할 경우,
+    //  null 아닌 값을 반환하여 에러 방지
+    fullName(root) {
+      console.log("fullName called!");
+      console.log(root);
+      return "hello!";
+    },
+  },
+  */
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
     },
   },
 };
